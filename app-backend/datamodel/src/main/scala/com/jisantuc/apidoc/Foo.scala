1266c69df6d7fd69a39f5ca428b2e519cb8ed527
James Santucci <james.santucci@gmail.com>
2019-10-15 09:40:18 -0500

    Make foo an ADT to see what happens

--- app-backend/datamodel/src/main/scala/com/jisantuc/apidoc/Foo.scala
+++ app-backend/datamodel/src/main/scala/com/jisantuc/apidoc/Foo.scala
@@ -1,15 +1,43 @@
 package com.jisantuc.apidoc.datamodel
 
+import cats.implicits._
 import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto._
+import io.circe.syntax._
 
-case class Foo(
+sealed trait Foo
+
+object Foo {
+
+  import SmallFoo._
+  import BigFoo._
+
+  implicit val encFoo: Encoder[Foo] = new Encoder[Foo] {
+
+    def apply(f: Foo) = f match {
+      case big: BigFoo     => big.asJson
+      case small: SmallFoo => small.asJson
+    }
+  }
+
+  implicit val decFoo: Decoder[Foo] =
+    Decoder[SmallFoo].widen or Decoder[BigFoo].widen
+}
+
+case class SmallFoo(x: Int) extends Foo
+
+object SmallFoo {
+  implicit val encSmall: Encoder[SmallFoo] = deriveEncoder
+  implicit val decSmall: Decoder[SmallFoo] = deriveDecoder
+}
+
+case class BigFoo(
     x: Int,
     y: String,
     z: Bar
-)
+) extends Foo
 
-object Foo {
-  implicit val encFoo: Encoder[Foo] = deriveEncoder
-  implicit val decFoo: Decoder[Foo] = deriveDecoder
+object BigFoo {
+  implicit val encBig: Encoder[BigFoo] = deriveEncoder
+  implicit val decBig: Decoder[BigFoo] = deriveDecoder
 }
